dfd91e14953fda867ff32b505227446bde72423c
Sergey <barclays1313@gmail.com>
2020-11-05 17:58:58 +0300

    feat(develop): create header

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,26 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ],
   },
 };
