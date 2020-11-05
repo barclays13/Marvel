3892cfcd2c9fdafc599bb93ab7d69c46e5c800ed
Sergey <barclays1313@gmail.com>
2020-11-06 01:27:45 +0300

    feat(develop): mobile version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import './style/style.less';
 
-console.log("13");
+import './style/style.less';
+import './style/mobile.less';
+
+const navbarToggler = document.querySelector('.navbar-toggler'),
+    headerNavbar = document.querySelector('.header__navbar');
+
+navbarToggler.addEventListener('click' , event => {
+    const targer = event.target;
+    // headerNavbar.className += ' active';
+})
