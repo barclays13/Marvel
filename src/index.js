b7c4f425edf26d4a41c59936b77b79f77ed5ce26
Sergey <barclays1313@gmail.com>
2020-11-05 12:33:36 +0300

    feature(global): setup project,  install packages, base file structure

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './style/style.less';
+
+console.log("13");
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,   
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
