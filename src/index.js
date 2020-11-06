179461f55c3df560981ac721cd29975634eef75e
leonid rudenkov <leo.rudenkov@gmail.com>
2020-11-06 13:36:38 +0300

    fix(deveolp): change stele.css, index.html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style/style.less';
 import './style/mobile.less';
+
+const headerListLink = document.querySelectorAll('.header__link'),
+      headerLinks = document.querySelector('.header__links');
+
+headerLinks.addEventListener('click', event => {
+  const target = event.target;
+  headerListLink.forEach( elem => {
+    if (elem.classList.contains("header__link_active")) {
+      elem.classList.remove("header__link_active");
+    }
+  });
+
+  target.classList.toggle("header__link_active");
+})
+
