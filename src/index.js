a2f9acbce20d576f736d7f729cb742e82c0f58b1
leonid rudenkov <leo.rudenkov@gmail.com>
2020-11-06 15:48:20 +0300

    feat(develop): add logic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import './style/style.less';
 import './style/mobile.less';
 
 const headerListLink = document.querySelectorAll('.header__link'),
-      headerLinks = document.querySelector('.header__links');
+      headerLinks = document.querySelector('.header__links'),
+      burgerBtn = document.querySelector('.burger'),
+      closeBtn = document.querySelector('.close'),
+      headerDesktop = document.querySelector('.header__desktop'),
+      headerMobile = document.querySelector('.header__mobile');
 
 headerLinks.addEventListener('click', event => {
   const target = event.target;
@@ -13,5 +17,22 @@ headerLinks.addEventListener('click', event => {
   });
 
   target.classList.toggle("header__link_active");
-})
+});
 
+burgerBtn.addEventListener('click', () => {
+  headerDesktop.classList.toggle('d-none');
+  headerMobile.classList.toggle('d-none');
+});
+
+closeBtn.addEventListener('click', () => {
+  headerDesktop.classList.toggle('d-none');
+  headerMobile.classList.toggle('d-none');
+});
+
+window.addEventListener('resize', () => {
+  const screenWidth = document.documentElement.clientWidth
+  if (screenWidth > 768 && headerDesktop.classList.contains('d-none')) {
+    headerDesktop.classList.toggle('d-none');
+    headerMobile.classList.toggle('d-none');
+  }
+})
