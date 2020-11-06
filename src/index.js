b819b1863c4c1543adce6516a5f4c615cfb87835
Sergey <barclays1313@gmail.com>
2020-11-06 08:59:42 +0300

    fix(develop): change style.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-
 import './style/style.less';
 import './style/mobile.less';
-
-const navbarToggler = document.querySelector('.navbar-toggler'),
-    headerNavbar = document.querySelector('.header__navbar');
-
-navbarToggler.addEventListener('click' , event => {
-    const targer = event.target;
-    // headerNavbar.className += ' active';
-})
